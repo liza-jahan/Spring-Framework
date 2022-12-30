9a3ff87d9e21fc7348271ff13959719a0459804e
Eakteker jahan Liza <74711092+liza-jahan@users.noreply.github.com>
2022-12-30 13:27:21 +0600

    Add files via upload

---
+++ T12PNameSpace/src/test/java/com/livingoncodes/spring/test3/TutorialEightF/AppTest.java
@@ -0,0 +1,20 @@
+package com.livingoncodes.spring.test3.TutorialEightF;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
