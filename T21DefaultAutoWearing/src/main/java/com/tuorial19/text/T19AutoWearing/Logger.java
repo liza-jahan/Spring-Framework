5df91be7e898470564a9604243a1c3adb59f7d3f
Eakteker jahan Liza <74711092+liza-jahan@users.noreply.github.com>
2023-01-05 01:16:24 +0600

    Add files via upload

---
+++ T21DefaultAutoWearing/src/main/java/com/tuorial19/text/T19AutoWearing/Logger.java
@@ -0,0 +1,37 @@
+package com.tuorial19.text.T19AutoWearing;
+
+public class Logger {
+	private ConsolWriter consolWriter;
+	private FileWriter fileWriter;
+
+	
+	
+	
+	
+
+	public LogWriter getConsolWriter() {
+		return consolWriter;
+	}
+
+	public void setConsolWriter(ConsolWriter consolWriter) {
+		this.consolWriter = consolWriter;
+	}
+
+	public LogWriter fileWriter() {
+		return fileWriter;
+	}
+
+	public void setFileWriter(FileWriter fileWriter) {
+		this.fileWriter = fileWriter;
+	}
+
+	public void writeFile(String text) {
+		fileWriter.write(text);
+
+	}
+
+	public void writeConsol(String text) {
+		consolWriter.write(text);
+	}
+
+}
