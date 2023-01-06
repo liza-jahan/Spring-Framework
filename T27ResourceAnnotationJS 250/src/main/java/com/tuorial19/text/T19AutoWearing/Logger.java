8c6ca104fcba9a0513f23fb68f22b3c5f1703e6f
Eakteker jahan Liza <74711092+liza-jahan@users.noreply.github.com>
2023-01-06 16:49:38 +0600

    Add files via upload

---
+++ T27ResourceAnnotationJS 250/src/main/java/com/tuorial19/text/T19AutoWearing/Logger.java
@@ -0,0 +1,34 @@
+package com.tuorial19.text.T19AutoWearing;
+
+import javax.annotation.Resource;
+
+
+public class Logger {
+	private ConsolWriter consolWriter;
+	private LogWriter fileWriter;
+	
+    // @Resource(name="Writer")
+	@Resource
+	public void setConsolWriter(ConsolWriter consolWriter) {
+		this.consolWriter = consolWriter;
+	}
+   @Resource(name="demoWriter")
+	public void setFileWriter(LogWriter fileWriter) {
+		this.fileWriter = fileWriter;
+	}
+
+	public void writeFile(String text) {
+		fileWriter.write(text);
+	}
+
+	public void writeConsol(String text) {
+
+		consolWriter.write(text);
+
+	}
+	/*
+	 * @PostConstruct public void init() { System.out.println("create bean"); }
+	 * 
+	 * public void destroy() { System.out.println("Destory bean"); }
+	 */
+}
