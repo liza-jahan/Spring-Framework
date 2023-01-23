e141f7b4460387d3d72879cdf3194d62252c5cea
Eakteker jahan Liza <74711092+liza-jahan@users.noreply.github.com>
2023-01-23 20:57:46 +0600

    Add files via upload

---
+++ T67Hibrante/src/com/livingoncodes/spring/web/dao/Notice.java
@@ -0,0 +1,56 @@
+package com.livingoncodes.spring.web.dao;
+
+import javax.validation.constraints.Size;
+
+public class Notice {
+	private int id;
+	@Size(min = 4, max = 100)
+	private String name;
+	private String email;
+
+	public Notice() {
+
+	}
+
+	public Notice(String name, String email) {
+		this.name = name;
+		this.email = email;
+	}
+
+	public Notice(int id, String name, String email) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Notice [id=" + id + ", name=" + name + ", email=" + email + "]";
+	}
+
+}
